1a0b903eb4518cdeb1052e2207481ba9838d98a5
Ben Kagan <kagan.benjamin@gmail.com>
2020-01-29 13:57:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file
+function findMatching(array, string) {
+    return array.filter(function (driver) { return driver.toLowerCase() === string.toLowerCase(); });
+}
+
+function fuzzyMatch(array, string) {
+    let stringLength = string.length;
+    return array.filter(function (driver) { return driver.slice(0, stringLength) === string; });
+}
+
+function matchName(array, string) {
+    return array.filter(function (driver) { return driver.name.toLowerCase() === string.toLowerCase(); });
+}
